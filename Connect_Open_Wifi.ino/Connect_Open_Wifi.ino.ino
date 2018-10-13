58a901976ec8051503ccffe8966f42813b4de784
AhsenBaig <AhsenBaig@gmail.com>
2018-10-12 20:40:30 -0700

    Inital Commit

---
+++ Connect_Open_Wifi.ino/Connect_Open_Wifi.ino.ino
@@ -0,0 +1,95 @@
+// #include WiFi Library
+#include <ESP8266WiFi.h>
+
+/* Serial Baud Rate */
+#define SERIAL_BAUD       9600
+/* Delay paramter for connection. */
+#define WIFI_DELAY        500
+/* Max SSID octets. */
+#define MAX_SSID_LEN      32
+/* Wait this much until device gets IP. */
+#define MAX_CONNECT_TIME  30000
+
+/* SSID that to be stored to connect. */
+char ssid[MAX_SSID_LEN] = "<Your_Wifi_Name";
+
+/* Scan available networks and sort them in order to their signal strength. */
+void scanAndSort() {
+  memset(ssid, 0, MAX_SSID_LEN);
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan complete!");
+  if (n == 0) {
+    Serial.println("No networks available.");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks discovered.");
+    int indices[n];
+    for (int i = 0; i < n; i++) {
+      indices[i] = i;
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (WiFi.RSSI(indices[j]) > WiFi.RSSI(indices[i])) {
+          std::swap(indices[i], indices[j]);
+        }
+      }
+    }
+    for (int i = 0; i < n; ++i) {
+      Serial.println("The strongest open network is:");
+      Serial.print(WiFi.SSID(indices[i]));
+      Serial.print(" ");
+      Serial.print(WiFi.RSSI(indices[i]));
+      Serial.print(" ");
+      Serial.print(WiFi.encryptionType(indices[i]));
+      Serial.println();
+      if(WiFi.encryptionType(indices[i]) == ENC_TYPE_NONE) {
+        memset(ssid, 0, MAX_SSID_LEN);
+        strncpy(ssid, WiFi.SSID(indices[i]).c_str(), MAX_SSID_LEN);
+        break;
+      }
+    }
+  }
+}
+
+void setup() {
+// Scan, Sort, and Connect to WiFi   
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Scanning for open networks...");
+   if(WiFi.status() != WL_CONNECTED) {
+    /* Clear previous modes. */
+    WiFi.softAPdisconnect();
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    delay(WIFI_DELAY);
+    /* Scan for networks to find open guy. */
+    scanAndSort();
+    delay(WIFI_DELAY);
+    /* Global ssid param need to be filled to connect. */
+    if(strlen(ssid) > 0) {
+      Serial.print("Connecting to ");
+      Serial.println(ssid);
+      /* No pass for WiFi. We are looking for non-encrypteds. */
+      WiFi.begin(ssid);
+      unsigned short try_cnt = 0;
+      /* Wait until WiFi connection but do not exceed MAX_CONNECT_TIME */
+      while (WiFi.status() != WL_CONNECTED && try_cnt < MAX_CONNECT_TIME / WIFI_DELAY) {
+        delay(WIFI_DELAY);
+        Serial.print(".");
+        try_cnt++;
+      }
+      if(WiFi.status() == WL_CONNECTED) {
+        Serial.println("");
+        Serial.println("Connection Successful!");
+        Serial.println("Your device IP address is ");
+        Serial.println(WiFi.localIP());
+      } else {
+        Serial.println("Connection FAILED");
+      }
+    } else {
+      Serial.println("No open networks available. :-(");  
+    }
+  }
+}
+
+void loop ()  {
+}
